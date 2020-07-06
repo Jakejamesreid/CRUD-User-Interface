64c6f7240033eb04b7f103981e61d2f1b05f8659
jakejamesreid <jakejamesreid@gmail.com>
2020-07-06 16:42:31 +0100

    Added option to update entry in database

--- mongo_project.py
+++ mongo_project.py
@@ -18,11 +18,11 @@ def mongo_connect(url):
 
 def show_menu():
     print()
-    print("Option 1. Create")
-    print("Option 2. Find")
-    print("Option 3. Update")
-    print("Option 4. Delete")
-    print("Option 5. Exit")
+    print("1. Add a record")
+    print("2. Find a record by name")
+    print("3. Edit a record")
+    print("4. Delete a record")
+    print("5. Exit")
     print("")
 
     option = input("Enter an option: ")
@@ -68,17 +68,34 @@ def find_record():
             if k != "_id":
                 print(k.capitalize() + ": " + v.capitalize())
 
+def edit_record():
+    doc = get_record()
+    if doc:
+        update_doc = {}
+        print()
+        for k,v in doc.items():
+            if k != "_id":
+                update_doc[k] = input(k.capitalize() + " [" + v + "] : ")
+
+                if update_doc[k] == "":
+                    update_doc[k] = v
+
+        try:
+            coll.update_one(doc, {"$set": update_doc})
+            print()
+            print("Document updated")
+        except:
+            print("Error accessing the database")
+
 def main_loop():
     while True:
         option = show_menu()
         if option == "1":
-            print("You have selected option 1")
             add_record()
         elif option == "2":
-            print("You have selected option 2")
             find_record()
         elif option == "3":
-            print("You have selected option 3")
+            edit_record()
         elif option == "4":
             print("You have selected option 4")
         elif option == "5":
